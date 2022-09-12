53ba07b5c6dd0984822e631d6357bcf2d647ba98
sam <pflegermendes@gmail.com>
2022-09-11 22:01:05 -0300

    a

--- projeto/bin/projeto.dart
+++ projeto/bin/projeto.dart
@@ -3,7 +3,10 @@ import 'dart:io';
 void main() {
   double karma;
   var action;
-  bool chave = false, continuar = false, livrosPassagem = false;
+  bool chave = false,
+      continuar = false,
+      livroProsseguir = false;
+  String continuarScape="1938";
 
   print("\n•̀.̫•́✧ Seja bem-vinde ao Don't Tell Her!");
 
@@ -27,34 +30,34 @@ void main() {
 
   while (continuar == false) {
     print("\n=============================================\n"
-        "A. Checar a janela.\nB. Checar a cômoda.\nC. Checar o vaso de vidro."
+        "A. Checar a janela.\nB. Checar a cômoda.\nC. Checar o vaso de vidro.\nD. Checar livros."
         "\n=============================================\n");
     action = stdin.readLineSync();
 
     switch (action) {
       case "A":
         print(
-            "Você se aproxima da antiga janela de madeira. Seus vidros embaçados e sujos de poeira revelam "
+            "\nVocê se aproxima da antiga janela de madeira. Seus vidros embaçados e sujos de poeira revelam "
                 "uma pequena estrada de terra batida que serpenteia entre grama e vegetação morta em direção a um pequeno bosque de árvores abandonadas pelo tempo.");
         break;
       case "B":
         if (chave == true) {
           print(
-              "Você se reaproxima da cômoda de pinho, seus engates de metal são frios ao toque, usando a chave enferrujada para abrir a gaveta "
+              "\nVocê se reaproxima da cômoda de pinho, seus engates de metal são frios ao toque, usando a chave enferrujada para abrir a gaveta "
                   "a resistência revela uma madeira há muito apodrecida. O cheiro de mofo invade seus pulmões "
                   "e em seu interior repousa um papel afetado pelo tempo, seus garranchos dizem:\n "
                   "╔══════════════════════════════════════════════════════════════════════════╗"
                   "\n    Minha cara, mando-lhe esta carta como um apelo.\n"
                   "\n    Nos ultimos dias ele apresenta um estado mais agitado que o habitual, "
                   "\n    não consego contê-lo, rogo por sua ajuda o mais rápido possível, "
-                  "\n    por favor, venha até aqui, \n"
+                  "\n    por favor, venha até aqui\n"
                   "\n               Atenciosamente, S.H.\n"
+                  "\n                           Dezenove de março de mil novecentos e oito.\n"
                   "╚══════════════════════════════════════════════════════════════════════════╝\n\n");
-               livrosPassagem = true;
-               continuar = true;
+          livroProsseguir = true;
         } else {
           print(
-              "Você se aproxima da cômoda de pinho, há uma gaveta ali e está trancada, é impossível abri-la.");
+              "\nVocê se aproxima da cômoda de pinho, há uma gaveta ali e está trancada, é impossível abri-la.");
         }
         break;
       case "C":
@@ -62,11 +65,19 @@ void main() {
             "Você se aproxima do vaso de vidro, seu corpo é repleto de ranhuras e algumas partes faltando. Em seu interior há uma chave misteriosa.");
         chave = true;
         break;
-    }
-  }
+      case "D":
+        print(
+            "\nVocê se aproxima da coleção de livros, suas velhas lambadas em capa dura revelam os títulos desgastados e banhados a ouro.\n\n"
+                "The Vampyre - 1819\nGoetia - 1904\nThe Hobbit - 1937\nFrankenstein - 1818\n\n");
+        if (livroProsseguir==true) {
+          print("Por algum motivo você prende sua atenção nas datas da coleção de livros. ");
 
-  if(livrosPassagem == true) {
-    print("CONTINUANDO HISTORIA");
+          funcCodeScape(continuarScape);
+        }else{
+          print("");
+        }
+        break;
+    }
   }
 }
 
@@ -113,5 +124,17 @@ funcMenu(bool inicio) {
             "colocar uma frase q ajude em um desafio aqui\n"
             "\n=============================================\n");
     }
+  }
+}
+
+funcCodeScape(String scape){
+  print("Informe a ordem numérica a qual deseja ordenar os livros.\n");
+  String? scape = stdin.readLineSync()!;
+  //scape = int.parse as String;
+
+  if(scape == "1938"){
+    print("usuario escapou");
+  }else{
+    print("usuario errou o codigo");
   }
 }
